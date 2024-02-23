b096b945e7fa66a68c8d7376b22eccc23ef77994
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-23 10:44:41 +0100

    Erabiltzailea ikusten da Index-ean

---
+++ Index_Arrunta.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <title>Urpera Piraguismoa</title>
+    <link rel="stylesheet" href="style.css">
+    <?php 
+
+     require 'connection.php';?>
+</head>
+<body>
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link href="https://fonts.googleapis.com/css?family=Lato|Nanum+Gothic:700|Raleway&display=swap"
+    rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" rel="stylesheet">
+<title>Urpera Kluba</title>
+
+</head>
+
+<header id="topHeader">
+<ul id="topInfo">
+
+    <li>info@urpera.com</li>
+
+</ul>
+
+<nav>
+<span class="logo">Erabiltzailea:  <?php echo ($_SESSION["inputemail"]) ? $_SESSION["inputemail"] : "Not logged in"; ?></span>
+    <div class="menu-btn-3" onclick="menuBtnFunction(this)">
+        <span></span>
+    </div>
+
+    <div class="mainMenu">
+
+        <a href="index.html"><span>Hasiera</span></a>
+        <a href="Egutegia.html"><span>Egutegia</span></a>
+        <a href="Login.php">Saioa hasi</a>
+
+    </div class="mainMenu">
+</nav>
+</header>
+
+<?php
+
+
+
+
+
+
+?>
+</body>
+</html>
--- check_login.php
+++ check_login.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // Verificar si se ha enviado el formulario
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Obtener las credenciales del formulario
@@ -11,12 +12,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $query = "SELECT * FROM erabiltzailea WHERE Email='$email' AND Pasahitza='$password'";
     $result = $conn->query($query);
 
+  
     // Verificar el resultado de la consulta
     if ($result->rowCount() > 0) {
         // Usuario autenticado correctamente
         $row = $result->fetch(PDO::FETCH_ASSOC);
         $userType = $row["Mota"];
-
+        $_SESSION["inputemail"] = $email;
         if ($userType == "Admin") {
             // Usuario Admin
             // Puedes redirigir a la página de administrador o realizar otras acciones
@@ -35,6 +37,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Cerrar la conexión a la base de datos
-    $conn=null;
+    
 }
 ?>
