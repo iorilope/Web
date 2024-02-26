ac71a94c18a8ca4384814092c2096ad41c7230e7
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-26 12:10:36 +0100

    Administratzaile atala falta da

---
+++ check_register.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener las credenciales del formulario
+    $izena = $_POST["fizena"];
+    $pasahitza = $_POST["fpasahitza"];
+    $abizena = $_POST["fabizena"];
+    $email = $_POST["femail"];
+
+    require 'connection.php';
+
+    /*MySQL*/
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "piraguismo";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+     // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+
+    $query = "INSERT INTO erabiltzailea (Izena, Abizena, Email, Pasahitza, Mota) VALUES ('$izena', '$abizena', '$email', '$pasahitza', 'Arrunta')";
+
+    if ($conn->query($query)) {
+        echo "<h2>Erabiltzaile berria sortu da</h2>";
+        header("Location: index.html");
+    } else {
+        echo "Error: " . $query . "<br>" . $conn->error;
+    }
+}
+?>
