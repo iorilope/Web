99a1f3f7116a4d5ed988a8bfa1fbec8b445dd88b
iorilope <146725975+iorilope@users.noreply.github.com>
2024-02-27 08:32:33 +0100

    Aplikazioa eginda responsive bakarrik falta

--- ProfilArrunta.php
+++ ProfilArrunta.php
@@ -19,6 +19,7 @@ $profilakresult = $profilakquery->fetchAll();
     <title>Urpera Piraguismoa</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="./css/tablestyle.css">
+    <link rel="stylesheet" href="./css/profila.css">
 
 </head>
 
@@ -48,30 +49,68 @@ $profilakresult = $profilakquery->fetchAll();
             </div>
 
             <div class="mainMenu">
-
-                <a href="Index_Arrunta.php"><span>Hasiera</span></a>
+            <a href="Index_Arrunta.php"><span>Hasiera</span></a>
                 <a href="Egutegia.html"><span>Egutegia</span></a>
                 <a href="Taldeak.php"><span>Taldeak</span></a>
                 <a href="piraguistak.php"><span>Piraguistak</span></a>
                 <a href="Txapelketak.php"><span>Txapelketak</span></a>
                 <a href="Klasifikazioa.php"><span>Klasifikazioa</span></a>
+                <a href="ProfilArrunta.php"><span>Profila</span></a>
                 <a href="logout.php">Saioa Itxi</a>
 
             </div class="mainMenu">
         </nav>
 
+        <section id="Txapelketak">
+            <div id="new-taldea-info">
+                <span></span>
+                <table class="styled-table">
+                    <thead>
+                        <tr>
+                            <th align="center">Izena</th>
+                            <th>Abizena</th>
+                            <th>Id</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        <?php foreach ($profilakresult as $index => $profila): ?>
+                            <tr>
+                                <td align="center">
+                                    <?php echo $profila["Izena"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $profila["Abizena"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $profila["Id"] ?>
+                                </td>
+                                <td>
+                                    <?php echo $profila["Email"] ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+
+                    </tbody>
+                </table>
+            </div>
+        </section>
         <section id="profila">
             <div id="new-taldea-info">
                 <span></span>
                 <h2>Profila editatu</h2>
+                <form action="profila2.php" method="post">
+    <div class="Menu">
+    </div>
 
-                <a class="btn" href="izenaaldatu.php">Izena aldatu</a>
-                <a class="btn" href="abizenaaldatu.php">Abizena aldatu</a>
-                <a class="btn" href="pasahitzaaldatu.php">Pasahitza aldatu</a>
-                <a class="btn" href="ezabatuarrunta.php">Erabiltzailea ezabatu</a>
-
-            </div>
+    <button type="submit" name="action" value="izenaaldatu" class="btn">Izena aldatu</button>
+    <button type="submit" name="action" value="abizenaaldatu" class="btn">Abizena aldatu</button>
+    <button type="submit" name="action" value="pasahitzaaldatu" class="btn">Pasahitza aldatu</button>
+    <button type="submit" name="action" value="ezabatuarrunta" class="btn">Erabiltzailea ezabatu</button>
+</form>
         </section>
+
 
 </body>
 
---
+++ profila2.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+require 'connection.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>Urpera Piraguismoa</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/tablestyle.css">
+    <link rel="stylesheet" href="./css/profila.css">
+    <link rel="stylesheet" href="./css/formdinamic.css">
+
+</head>
+
+<body>
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link href="https://fonts.googleapis.com/css?family=Lato|Nanum+Gothic:700|Raleway&display=swap"
+            rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css" rel="stylesheet">
+        <title>Urpera Kluba</title>
+
+    </head>
+
+    <header id="topHeader">
+
+        <nav>
+            <span class="logo">Erabiltzailea:
+                <?php echo ($_SESSION["inputemail"]) ? $_SESSION["inputemail"] : "Ez da saioa hasi"; ?>
+            </span>
+            <div class="menu-btn-3" onclick="menuBtnFunction(this)">
+                <span></span>
+            </div>
+
+            <div class="mainMenu">
+            <a href="Index_Arrunta.php"><span>Hasiera</span></a>
+                <a href="Egutegia.html"><span>Egutegia</span></a>
+                <a href="Taldeak.php"><span>Taldeak</span></a>
+                <a href="piraguistak.php"><span>Piraguistak</span></a>
+                <a href="Txapelketak.php"><span>Txapelketak</span></a>
+                <a href="Klasifikazioa.php"><span>Klasifikazioa</span></a>
+                <a href="ProfilArrunta.php"><span>Profila</span></a>
+                <a href="logout.php">Saioa Itxi</a>
+
+
+            </div class="mainMenu">
+        </nav>
+
+        <section id="profila">
+            <div id="new-taldea-info">
+                <span></span>
+                <h2>Profila editatu</h2>
+
+        </section>
+        <?php
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+    $fid = isset($_POST['fid']) ? $_POST['fid'] : null;
+
+
+    switch ($action) {
+        case 'izenaaldatu': 
+            ?>
+            <form method="POST" action="izena2.php">
+     
+                Izen berria: <input type="text" name="izen_berria"><br>
+                <input type="submit" />
+            </form>
+            <?php
+            break;
+        case 'abizenaaldatu':
+            ?>
+            <form method="POST" action="abizena2.php">
+             
+                Abizen berria: <input type="text" name="abizen_berria"><br>
+                <input type="submit" />
+            </form>
+            <?php
+            break;
+        case 'pasahitzaaldatu':
+            ?>
+            <form method="POST" action="pasahitza2.php">
+      
+                Pasahitz berria: <input type="password" name="pasahitz_berria"><br>
+                <input type="submit" />
+            </form>
+            <?php
+            break;
+        case 'ezabatuarrunta':
+            ?>
+            <form method="POST" action="Ezabatu.php" id="deleteForm">
+
+                <button type="submit" onclick="return confirm('¿Seguro zaude ezabatu nahi duzula?')">Ezabatu</button>
+                <button type="button" onclick="ezabatuez()">Ezeztatu</button>
+            </form>
+            <?php
+            break;
+        default:
+   
+            break;
+    }
+}
+?>
+</body>
+<script>
+    function ezabatubai() {
+        var confirmacion = confirm("¿Seguro zaude ezabatu nahi duzula?");
+        if (confirmacion) {
+            document.getElementById('deleteForm').submit();
+        }
+    }
+
+    function ezabatuez() {
+
+        window.location.href = 'IndexAdmin.php';
+    }
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="ScriptIT.js"></script>
+<script src="./script.js"></script>
+
+</html>
